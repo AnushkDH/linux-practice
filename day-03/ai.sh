a6222998bfebf991e8221bd88d723da41efb1244
Anushk Dhokne <130216680+AnushkDH@users.noreply.github.com>
2026-03-27 14:56:28 +0530

    This is my first shell script

--- day-03/ai.sh
+++ day-03/ai.sh
@@ -1,26 +1,27 @@
 echo =========================================
 echo           SYSTEM INFORMATION
 echo =========================================
+echo ""
 
 echo "Memory Usage: "free -h""
 echo "-----------------------------------------"
-
 free -h
+echo ""
 
 echo "System Info: "uname -a""
 echo "-----------------------------------------"
-
 uname -a
+echo ""
 
 echo "Disk Usage: "df -h""
 echo "-----------------------------------------"
-
 df -h
+echo ""
 
 echo "Logged in Users: "who""
 echo "-----------------------------------------"
-
 who
+echo ""
 
 echo "Directory Size: "du -h""
 echo "-----------------------------------------"
