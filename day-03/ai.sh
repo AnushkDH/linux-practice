c90e690859068db0f07f582e381657a39dc111a5
Anushk Dhokne <130216680+AnushkDH@users.noreply.github.com>
2026-03-27 14:53:44 +0530

    this my sheel script

---
+++ day-03/ai.sh
@@ -0,0 +1,30 @@
+echo =========================================
+echo           SYSTEM INFORMATION
+echo =========================================
+
+echo "Memory Usage: "free -h""
+echo "-----------------------------------------"
+
+free -h
+
+echo "System Info: "uname -a""
+echo "-----------------------------------------"
+
+uname -a
+
+echo "Disk Usage: "df -h""
+echo "-----------------------------------------"
+
+df -h
+
+echo "Logged in Users: "who""
+echo "-----------------------------------------"
+
+who
+
+echo "Directory Size: "du -h""
+echo "-----------------------------------------"
+
+du -hd
+~                                                                                                                                                                                                                                                 
+~        
